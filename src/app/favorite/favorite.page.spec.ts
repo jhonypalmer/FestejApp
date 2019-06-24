185832a29b79ff64c82df57719547e5b6cace850
Jhony Palmer <jhonypalmer@hotmail.com>
2019-06-24 13:58:20 -0300

    Update page Favorite
    Substituindo page tab3 por favorite

--- src/app/favorite/favorite.page.spec.ts
+++ src/app/favorite/favorite.page.spec.ts
@@ -1,21 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Tab3Page } from './tab3.page';
+import { FavoritePage } from './favorite.page';
 
-describe('Tab3Page', () => {
-  let component: Tab3Page;
-  let fixture: ComponentFixture<Tab3Page>;
+describe('FavoritePage', () => {
+  let component: FavoritePage;
+  let fixture: ComponentFixture<FavoritePage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab3Page],
+      declarations: [FavoritePage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(Tab3Page);
+    fixture = TestBed.createComponent(FavoritePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
