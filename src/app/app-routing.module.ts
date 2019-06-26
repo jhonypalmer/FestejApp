a8af59870f61dce5abce9b941dd3f2f5bc70e838
Jhony Palmer <jhonypalmer@hotmail.com>
2019-06-26 02:45:50 -0300

    Create page Events and Update page Search
    Create page Events and Update page Search for Front-End

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'events', loadChildren: './events/events.module#EventsPageModule' }
 ];
 @NgModule({
   imports: [
