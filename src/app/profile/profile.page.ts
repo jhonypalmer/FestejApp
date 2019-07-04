2c2287e38993aeb11fe917394825e587f4d8b847
Marconim <marconi123@topzera.com>
2019-07-04 13:54:44 -0300

    Tela de login

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  email = ''
+  password = ''
+  passwordConfirmation = ''
+  login = true
+  cadastro = false
+  perfil = false
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  cadastroClick() {
+    this.login = false;
+    this.cadastro = true;
+  }
+
+  loginClick() {
+    this.login = true;
+    this.cadastro = false;
+  }
+
+  changeEmail(event) {
+    this.email = event.target.value
+  }
+
+  changePassword(event) {
+    this.password = event.target.value
+  }
+
+  changePasswordConfirmation(event) {
+    this.passwordConfirmation = event.target.value
+  }
+
+  efetuaLogin() {
+    alert(this.email + ' | ' + this.password)
+    this.login = false;
+    this.cadastro = false;
+    this.perfil = true
+  }
+
+  efetuaCadastro() {
+    alert(this.email + ' | ' + this.password + ' | ' + this.passwordConfirmation)
+    this.login = false;
+    this.cadastro = false;
+    this.perfil = true
+  }
 }
